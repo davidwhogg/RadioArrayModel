227ed62d6a1a8cc44a33460d62186f2fbee912f0
David W. Hogg <dwhogg@gmail.com>
2012-06-10 19:24:17 +0200

    start

---
+++ code/sandbox.py
@@ -0,0 +1,8 @@
+"""
+This file is part of the Radio Array Model project.
+
+Copyright 2012 David W. Hogg (NYU).
+
+It contains a playground for building intuitions about radio
+interferometers.
+"""
